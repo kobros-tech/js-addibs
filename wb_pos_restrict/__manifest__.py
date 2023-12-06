3add8ebf08255ef71d45d1b56bdf0ed9afa6c676
kobros-tech <alkobroslymohamed@gmail.com>
2023-12-06 18:50:24 +0200

    hide the buttons on the POS screen

---
+++ wb_pos_restrict/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Weblrarns Point of Sale Development Tutorial (Restriction)",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "xml/view.xml",
+    ],
+    "assets": {
+        "point_of_sale.assets": [
+            "wb_pos_restrict/static/src/js/wb_sample_button.js",
+            "wb_pos_restrict/static/src/xml/wb_sample_button.xml",
+        ]
+    }
+}
