d19114dba9547712b4a277b8e8fa853726c08efd
kobros-tech <alkobroslymohamed@gmail.com>
2023-12-06 15:54:01 +0200

    Create Click Events for POS Buttons

--- wb_pos/static/src/js/wb_sample_button.js
+++ wb_pos/static/src/js/wb_sample_button.js
@@ -3,9 +3,19 @@ odoo.define("wb_pos.WBSampleButton", function(require){
     
     const PosComponent = require("point_of_sale.PosComponent");
     const ProductScreen = require("point_of_sale.ProductScreen");
-    const Registries = require("point_of_sale.Registries")
+    const Registries = require("point_of_sale.Registries");
+    const {useListener} = require("@web/core/utils/hooks");
 
     class WBSampleButton extends PosComponent {
+
+        setup(){
+            super.setup();
+            useListener("click", this.wb_sample_button_click);
+        }
+
+        wb_sample_button_click(){
+            console.log("Hello this is button click event pressed.......");
+        }
 
     }
 
