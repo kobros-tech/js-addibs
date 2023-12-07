7e31304e4b438218729559766688bf5e016eb8ff
kobros-tech <alkobroslymohamed@gmail.com>
2023-12-07 10:36:06 +0200

    Create a Popup in POS

--- wb_pos/static/src/js/wb_sample_button.js
+++ wb_pos/static/src/js/wb_sample_button.js
@@ -13,7 +13,57 @@ odoo.define("wb_pos.WBSampleButton", function(require){
             useListener("click", this.wb_sample_button_click);
         }
 
-        wb_sample_button_click(){
+        async wb_sample_button_click(){
+
+            // ERROR POPUP
+
+            // this.showPopup("ErrorPopup", {
+            //     title: "Error Message",
+            //     body: "The simple error message screen.",
+            // });
+
+
+            // CONFIRM POPUP
+
+            const { confirmed } = await this.showPopup("ConfirmPopup",{
+                title: "Confirm Popup",
+                body: "Are you really happy with Angel?",
+                confirmText: "Yes",
+                cancelText: "No",
+            });
+            console.log("confirm button: ", confirmed);
+
+
+            // OFFLINE ERROR POPUP
+
+            // this.showPopup("OfflineErrorPopup", {
+            //     title: "Odoo Error",
+            //     body: "Hey this is test popup screen, don't take seriously!",
+            // });
+
+
+            // SELECTION POPUP
+
+            // const { confirmed, payload: selectedOption } = await this.showPopup("SelectionPopup", {
+            //     title: "Are you a good JS developer?",
+            //     list: [
+            //         {"id":0, "label":'Yes', "item":"You are excellent!"},
+            //         {"id":1, "label":'No', "item":"Good luck!"},
+            //         {"id":2, "label":'Not Sure', "item":"Keep up!"},
+            //     ],
+            // });
+            // console.log(confirmed);
+            // console.log(selectedOption);
+
+
+            // CLOSE POS POPUP
+
+            // const info = await this.env.pos.getClosePosInfo();
+            // this.showPopup("ClosePosPopup", {
+            //     info: info,
+            //     keepBehind: true,
+            // });
+
             console.log("Hello this is button click event pressed.......");
         }
 
